022d40c140ca46add75695cd3e6e43a940c691c2
Andrew Sisley <a.j.sisley@hotmail.com>
2025-03-18 11:07:52 -0400

    PR FIXUP - Expand Stateful documentation

--- action/stateful.go
+++ action/stateful.go
@@ -4,6 +4,10 @@ package action
 //
 // It is commonly used by [Action]s, and the state will be
 // set by various functions within the source testing packages.
+//
+// For example, [github.com/sourcenetwork/testing.ExecuteS] calls
+// [Stateful.SetState] for every action provided to it, allowing those
+// given actions to access the test state during test execution.
 type Stateful[TState any] interface {
 	// SetState overwrites currently held state with the given value.
 	SetState(TState)
